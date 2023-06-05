8c4cedeb0d545ce9f2012aebdc7050d3e68d8d0e
Efe ERKEN <efe.erken@math.unistra.fr>
2023-06-05 17:08:24 +0200

    :wrench: UPDATE: Fine tune math
    Adjusted and tuned the math and equation management further.

--- src/translatex/marker.py
+++ src/translatex/marker.py
@@ -1,10 +1,13 @@
-from TexSoup import TexSoup
-from TexSoup.data import *
-from TexSoup.data import TexUnNamedEnv, TexExpr
 from typing import Dict, List, Type
 
+from TexSoup import TexSoup
+from TexSoup.data import *
+from TexSoup.data import TexUnNamedEnv
+
 MATH_ENVS: List[Type[TexUnNamedEnv]] = [TexMathEnv, TexMathModeEnv, TexDisplayMathEnv, TexDisplayMathModeEnv]
+MATH_NAMED_ENVS: List[str] = ["equation", "cases", "array", "matrix", "pmatrix", "bmatrix", "Bmatrix", "vmatrix", "Vmatrix"]
 TEXT_COMMANDS: List[str] = ["text", "textsf", "textrm", "textnormal"]
+SKIPPED_COMMANDS: List[str] = ["label", "ref", "cite", "href", "inputencoding", "verb", "lstinputlisting"]
 
 
 class Marker:
@@ -32,43 +35,37 @@ class Marker:
             node.name = self.__next_marker__()
             self.marker_store.update({self.marker_count: previous_name})
         elif type(node.expr) is TexCmd:
-            if node.name in ["label", "ref"]:
+            if node.name in SKIPPED_COMMANDS:
                 return
             node.name = self.__next_marker__()
             self.marker_store.update({self.marker_count: previous_name})
 
-    def __mark_node_contents__(self, node: TexNode, replace_range: range = None) -> None:
+    def __mark_node_contents__(self, node: TexNode, original_contents_size: int = 0,
+                               replace_range: range = None) -> None:
         # TODO: Test if lists need deep copying
-        if replace_range is None:
+        if (original_contents_size != 0) ^ (replace_range is not None):
+            raise ValueError("Either supply both optional parameters or none of them")
+        if original_contents_size == 0 and replace_range is None:
             previous_contents: list = node.contents
             node.contents = [self.__next_marker__()]
             self.marker_store.update({self.marker_count: previous_contents})
         else:
-            previous_contents: list = node.contents[replace_range.start:replace_range.stop]
-
-            templs = list(node.contents)
-            del templs[replace_range.start:replace_range.stop]
-
-            # del node.contents[replace_range.start:replace_range.stop]
-
-            # node.contents = node.contents[:replace_range.start] + node.contents[replace_range.stop:]
-
-            # index = replace_range.start
-            # for __ in replace_range:
-            #     node.contents.pop(index)
-
-            # node.contents.insert(replace_range.start, self.__next_marker__())
-
-            templs.insert(replace_range.start, self.__next_marker__())
-            # templs[1] = TexExpr(templs[1].name, (templs[1].args, ))
-            templs[1] = str(templs[1]) # to be continued... (expression stays a string and doesn't get recursed into later)
-            node.contents = templs
+            current_contents_size = len(node.contents)
+            adjustment_difference = original_contents_size - current_contents_size
+            adjusted_replace_range = range(replace_range.start - adjustment_difference,
+                                           replace_range.stop - adjustment_difference)
+            previous_contents: list = node.contents[adjusted_replace_range.start:adjusted_replace_range.stop]
+            new_contents = list(node.contents)
+            del new_contents[adjusted_replace_range.start:adjusted_replace_range.stop]
+            new_contents.insert(adjusted_replace_range.start, self.__next_marker__())
+            # Turn every TexNode into a TexExpr since TexSoup wants it that way
+            # this also updates the syntax tree and recursion into nodes stays possible
+            new_contents = [x.expr if type(x) is TexNode else x for x in new_contents]
+            node.contents = new_contents
             self.marker_store.update({self.marker_count: previous_contents})
 
     def __traverse_ast_aux__(self, node: TexNode):
-        if len(node.children) == 0:
-            self.__mark_node_name__(node)
-        elif type(node.expr) in MATH_ENVS:
+        if type(node.expr) in MATH_ENVS or node.name in MATH_NAMED_ENVS:
             continue_recursion = False
             for descendant in node.descendants:
                 if type(descendant) is TexNode and descendant.name in TEXT_COMMANDS:
@@ -84,16 +81,24 @@ class Marker:
                     elif start != -1 and type(content) is TexNode and content.name in TEXT_COMMANDS:
                         ranges_to_mark.append(range(start, i))
                         start = -1
-                    # else:
-                    #     start = i
+                    elif len(node.contents) - 1 == i:
+                        ranges_to_mark.append(range(start, i))
+
+                original_contents_size: int = len(node.contents)
                 for range_to_mark in ranges_to_mark:
-                    self.__mark_node_contents__(node, range_to_mark)
+                    self.__mark_node_contents__(node, original_contents_size, range_to_mark)
 
                 for current_node in node.children:
                     self.__traverse_ast_aux__(current_node)
+                if type(node) is TexNamedEnv:
+                    self.__mark_node_name__(node)
 
             else:
                 self.__mark_node_contents__(node)
+                # TODO: implement way to completely mark and replace named math environment
+
+        elif len(node.children) == 0:
+            self.__mark_node_name__(node)
         else:
             for current_node in node.children:
                 self.__traverse_ast_aux__(current_node)
@@ -103,123 +108,7 @@ class Marker:
         self.__traverse_ast_aux__(self.soup.find("document"))
 
 
-m = Marker(r"""
-\documentclass{article}
-\usepackage[T1]{fontenc}
-\usepackage[french]{babel}
-\usepackage{amsthm,amssymb,amsmath,xcolor}
-\usepackage{hyperref}
-\hypersetup{
-	colorlinks,
-	linkcolor={red},
-	urlcolor={purple}
-}
-
-\newtheorem{definition}{Définition}
-\newtheorem{theorem}[definition]{Théorème}
-\newtheorem{remark}[definition]{Remarque}
-
-\begin{document}
-	\author{Efe ERKEN}
-	\date{octobre 2021}
-	\title{Le \LaTeX\ c'est bon, mangez-en}
-	\maketitle
-	
-	\section{Introduction}
-	Depuis la nuit des temps, ou presque, l’humanité a cherché à rendre compte de \underline{vérités universelles}. Parmi les folles formes foisonnantes, nous en retiendrons trois.
-	\label{sec:label1}
-	
-	\begin{definition}
-	\textbf{Loi zozotérique.} Quelle que soit l’absurdité d’une proposition, il existera toujours au moins 72\% de la population pour y croire.
-	\label{def:label1}
-	\end{definition}
-
-	\begin{definition}
-	\textbf{Loi tactique.} La pataphysique rit.
-	\label{def:label2}
-	\end{definition}
-
-	\begin{definition}
-	\textbf{Loi des piques.} Un fois ça va, dix fois bonjour les dégâts.
-	\label{def:label3}
-	\end{definition}	
-
-	\noindent Dans cet article nous proposons :
-	\begin{enumerate}
-		\item une approche \textbf{homogène} permettant d’affiner l’estimation de la proportion zozotérique $\Lambda\,(\mathcal{P})$ en fonction de la proposition $\mathcal{P}$ ;
-		\item une implémentation \textsc{Python} d’un algorithme de calcul effectif de $\Lambda\,(\mathcal{P})$.
-		\item Le grand oral, en 69 mots.
-	\end{enumerate}
-	
-	\begin{theorem}
-		Étant donnée une proposition $\mathcal{P}$, la proportion zozotérique $\Lambda\,(\mathcal{P})$ égale sa crédulance relative à la population.
-		\label{theo:label1}
-	\end{theorem}
-	
-	\begin{theorem}
-		Soit Pop un échantillon de population bleue et $\mathcal{P}$ une proposition homogène exprimée universellement par quantification imbriquée. Alors
-		\[
-		\Lambda\,(\mathcal{P}) \geqslant \, \; \sum_{72}^{10} \frac{\int_{-151}^{69} - \frac{3\mu}{2} + 4x - \pi + 2 \, \: \; \mathrm{d}x}{\sum_{10}^{72} - 5\alpha + \frac{x}{7} + 7 - \frac{18}{7\sigma} - \frac{5}{\alpha}} \, \: \;  \mathrm{,}
-		\text{hello there}
-		\]
-		avec égalité presque sûrement si, et seulement si, $\mathcal{P}$ est la proposition \og brouillard en matinée, belle et claire journée \fg.
-		\label{theo:label2}
-	\end{theorem}
-	
-	\begin{remark}
-		La Section \ref{sec:label2} contient la démonstration du Théorème \ref{theo:label2}.
-	\end{remark}
-	
-	\begin{remark}
-		En observant que ce théorème est en fait effectif, nous en déduisons également un algorithme de complexité $O(n(\mathcal{P}))$, où $n(\mathcal{P})$ est le nombre de caractères nécessaire à exprimer $\mathcal{P}$ en \href{https://lolcode.org/}{\textsc{Lolcode}}.
-	\end{remark}
-	
-	\section{Preuve du Théorème \ref{theo:label2}}
-	Les détails sont laissés au lecteur. À la place, révisons un peu nos classiques.
-	\label{sec:label2}
-	
-	\subsection{Liste des personnages}
-	\label{subsec:label1}
-	
-	\begin{description}
-		\item[Don Fernand :] premier roi de Castille
-		\item[Donna Urraque :] infante de Castille
-		\item[Don Diègue :] \emph{père de don Rodrigue}
-		\item[Don Gomès :] \emph{comte de Gormas et père de Chimène}
-		\item[Don Rodrigue :] \textsc{amant de Chimène}
-		\item[Don Sanche :] \textsc{amoureux de Chimène}
-		\item[Don Arias :] \textbf{gentilhomme castillan}
-		\item[Don Alonse :] \emph{gentilhomme castillan}
-		\item[Chimène :] \emph{fille de don Gomès}
-		\item[Léonor :] \textbf{gouvernante de l’infante}
-		\item[Elvire :] gouvernante de Chimène
-		\item[Le page :] \textbf{un page de l’infante}
-	\end{description}
-	
-	\subsection{Acte 10, scène 151}
-	\label{subsec:label2}
-	
-	\begin{itemize}
-		\item[$\blacksquare$] \textbf{Don Rodrigue} (\emph{absolument désenchantée}) \\
-		\textsf{Mon honneur lui fera mille autres ennemis.}
-		\item[$\blacksquare$] \textbf{Don Sanche} (\emph{dans un état de sidération intersidérale}) \\
-		\textsf{Ma vengeance est perdue et mes desseins trahis :}
-		\item[$\blacksquare$] \textbf{Chimène} (\emph{le ton enjoué}) \\
-		\textsf{Le guerrier sans courage et le perfide amant.}
-		\item[$\blacksquare$] \textbf{Chimène} (\emph{le visage défait}) \\
-		\textsf{Rodrigue a du courage.}
-		\item[$\blacksquare$] \textbf{Don Alonse} (\emph{absolument désenchantée}) \\
-		\textsf{Mais pour ne troubler pas une si belle flamme ;}
-		\item[$\blacksquare$] \textbf{Léonor} (\emph{d’une voix rauque}) \\
-		\textsf{On les laisse passer ; tout leur paraît tranquille ;}
-		\item[$\blacksquare$] \textbf{Don Rodrigue} (\emph{ regard incisif}) \\
-		\textsf{Ma main seule du mien a su venger l’offense,}
-		\item[$\blacksquare$] \textbf{Chimène} (\emph{le ton enjoué}) \\
-		\textsf{Dans la nuit qui survient troublerait trop la ville :}
-	\end{itemize}
-	
-\end{document}
-""")
-
+with open("../../examples/translatex.tex") as f:
+    m = Marker(f.read())
 m.traverse_ast()
 print(str(m))
