3aaefccd94369d4215a4c5a993eeb9557c27ca70
Efe ERKEN <efe.erken@math.unistra.fr>
2023-06-16 16:41:29 +0200

    :sparkles: NEW: Add small demo
    Added a quick and dirty demo to show advancements until now.

--- src/translatex/main.py
+++ src/translatex/main.py
@@ -1,7 +1,11 @@
 """Main module."""
 
 import argparse
+import subprocess
 import sys
+
+from translatex.marker import Marker
+from translatex.tokenizer import Tokenizer
 
 
 def main():
@@ -12,7 +16,32 @@ def main():
 
     print("Arguments: " + str(args._))
     print("Replace this message by putting your code into "
-          "translatex.translatex.main")
+          "translatex.main.main")
+
+
+    parser.add_argument('base_file', help="Name of the LaTeX source file")
+    args = parser.parse_args()
+    base_file: str = args.base_file
+    base_file = base_file.split(".tex")[0]
+    with open(f"{base_file}.tex") as f:
+        m = Marker(f.read())
+    m.mark()
+    with open(f"{base_file}_marked.tex", "w+") as f:
+        f.write(m.marked_latex)
+    t = Tokenizer(m.marked_latex)
+    t.tokenize()
+    with open(f"{base_file}_tokenized.tex", "w+") as f:
+        f.write(t.tokenized_string)
+    subprocess.run(['code', "--wait", f"{base_file}_tokenized.tex"])
+    with open(f"{base_file}_tokenized.tex") as f:
+        t.tokenized_string = f.read()
+    t.detokenize()
+    m.marked_latex = t.marked_string
+    m.unmark()
+    with open(f"{base_file}_result.tex", "w+") as f:
+        f.write(m.unmarked_latex)
+    print("Process complete")
+
     return
 
 
