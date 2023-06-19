f0327645d6a14bc23f846c7506e8cbb2d30ce947
Efe ERKEN <efe.erken@math.unistra.fr>
2023-06-19 17:14:25 +0200

    :memo: DOC: Add doc to package
    Added documentation to the top level TransLaTeX package.

--- src/translatex/__init__.py
+++ src/translatex/__init__.py
@@ -1,3 +1,13 @@
-"""Top-level package for TransLaTeX."""
+"""TransLaTeX is a program which aims to translate LaTeX source files (``.tex``) from one human language to
+another using automatic translators.
 
-__version__ = '0.0.1'
+The purpose is to extract text from LaTeX source files using a tokenization technique without including any commands
+or tags so that the result can be passed to an automatic translator like DeepL or Google Translate without messing up
+the LaTeX file. The objective is to have the same LaTeX file with only the text in another specified language which
+still compiles perfectly and visually intact. A single, simple tool to translate LaTeX.
+
+Apart from the main use case, the marking, tokenization and text extraction can be useful in other contexts such as
+manual translation by a professional human translator or parsing.
+"""
+
+__version__ = '0.0.2'
