e9a11a9aa2a9ab8724fbdd41f966c54bb4d1f5e1
Efe ERKEN <efe.erken@math.unistra.fr>
2023-06-23 11:01:18 +0200

    :wrench: UPDATE: Add the whole process
    Added a first iteration of the whole process pipeline in the main script.

--- src/translatex/main.py
+++ src/translatex/main.py
@@ -4,23 +4,13 @@ import argparse
 import subprocess
 import sys
 
+from translatex.preprocessor import Preprocessor
 from translatex.marker import Marker
 from translatex.tokenizer import Tokenizer
+from translatex.translator import Translator
 
 
-def main():
-    """Console script for translatex."""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "translatex.main.main")
-
-
-    parser.add_argument('base_file', help="Name of the LaTeX source file")
-    args = parser.parse_args()
+def demo(args):
     base_file: str = args.base_file
     base_file = base_file.split(".tex")[0]
     with open(f"{base_file}.tex") as f:
@@ -42,7 +32,56 @@ def main():
         f.write(m.unmarked_latex)
     print("Process complete")
 
-    return
+
+def translatex(args):
+    base_file: str = args.base_file
+    base_file = base_file.split(".tex")[0]
+    with open(f"{base_file}.tex") as f:
+        p = Preprocessor(f.read())
+    p.process()
+    if args.debug:
+        with open(f"{base_file}_processed.tex", "w+") as f:
+            f.write(p.processed_latex)
+    m = Marker.from_preprocessor(p)
+    m.mark()
+    if args.debug:
+        with open(f"{base_file}_marked.tex", "w+") as f:
+            f.write(m.marked_latex)
+    t = Tokenizer.from_marker(m)
+    t.tokenize()
+    if args.debug:
+        with open(f"{base_file}_tokenized.tex", "w+") as f:
+            f.write(t.tokenized_string)
+    a = Translator.from_tokenizer(t)
+    a.translate()
+    if args.debug:
+        with open(f"{base_file}_translated.tex", "w+") as f:
+            f.write(a.translated_string)
+    t.update_from_translator(a)
+    t.detokenize()
+    m.update_from_tokenizer(t)
+    m.unmark()
+    p.update_from_marker(m)
+    p.rebuild()
+    with open(f"{base_file}_result.tex", "w+") as f:
+        f.write(p.unprocessed_latex)
+    print("Process complete")
+
+
+def main():
+    """Console script for translatex."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "translatex.main.main")
+
+    parser.add_argument("base_file", help="Name of the LaTeX source file")
+    parser.add_argument("-d", "--debug", action="store_true", help="Name of the LaTeX source file")
+    args = parser.parse_args()
+    translatex(args)
 
 
 if __name__ == "__main__":
