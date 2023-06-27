a307546c4d44a30883b1afbe467c26c6fc4e5d2f
Efe ERKEN <efe.erken@math.unistra.fr>
2023-06-27 17:33:22 +0200

    :wrench: UPDATE: Add verbosity
    Added more verbosity and more intermediary debug file generation, fixed doc.

--- src/translatex/main.py
+++ src/translatex/main.py
@@ -1,4 +1,10 @@
-"""Translatex: A LaTeX translator."""
+"""Translatex: A LaTeX translator.
+
+The entry point to the program. The given LaTeX file is run through multiple processes such as preprocessing, marking,
+and tokenization to replace all LaTeX syntax constructs by tokens that ideally won't be modified by automatic
+translators. That, for the purpose of translating LaTeX source code automatically to any specified language while
+keeping it error free and compilable at the end, resulting in the same looking file.
+"""
 
 import argparse
 import sys
@@ -11,7 +17,7 @@ from translatex.tokenizer import Tokenizer
 from translatex.translator import Translator
 
 DEFAULT_INTER_FILE_PRE: str = "_"
-DEFAULT_INTER_FILE_EXT: str = "txt"
+DEFAULT_INTER_FILE_EXT: str = ".txt"
 
 
 def translatex(args: argparse.Namespace) -> None:
@@ -25,8 +31,10 @@ def translatex(args: argparse.Namespace) -> None:
         args.outfile.write(p.processed_latex)
         sys.exit()
     if args.debug:
-        with open(f"{base_file}_processed." + DEFAULT_INTER_FILE_EXT, "w+") as f:
+        with open(f"{base_file}_processed" + DEFAULT_INTER_FILE_EXT, "w+") as f:
             f.write(p.processed_latex)
+        with open(f"{base_file}_indicator_store" + DEFAULT_INTER_FILE_EXT, "w+") as f:
+            f.write(p.dump_store())
     m = Marker.from_preprocessor(p)
     if args.marker_format:
         m.marker_format = args.marker_format
@@ -37,8 +45,10 @@ def translatex(args: argparse.Namespace) -> None:
         args.outfile.write(m.marked_latex)
         sys.exit()
     if args.debug:
-        with open(f"{base_file}_marked." + DEFAULT_INTER_FILE_EXT, "w+") as f:
+        with open(f"{base_file}_marked" + DEFAULT_INTER_FILE_EXT, "w+") as f:
             f.write(m.marked_latex)
+        with open(f"{base_file}_marker_store" + DEFAULT_INTER_FILE_EXT, "w+") as f:
+            f.write(m.dump_store())
     t = Tokenizer.from_marker(m)
     if args.token_format:
         t.token_format = args.token_format
@@ -49,22 +59,24 @@ def translatex(args: argparse.Namespace) -> None:
         args.outfile.write(t.tokenized_string)
         sys.exit()
     if args.debug:
-        with open(f"{base_file}_tokenized." + DEFAULT_INTER_FILE_EXT, "w+") as f:
+        with open(f"{base_file}_tokenized" + DEFAULT_INTER_FILE_EXT, "w+") as f:
             f.write(t.tokenized_string)
+        with open(f"{base_file}_token_store" + DEFAULT_INTER_FILE_EXT, "w+") as f:
+            f.write(t.dump_store())
     a = Translator.from_tokenizer(t)
     if not args.dry_run:
         a.translate()
-        if args.verbose:
-            print("---- Translator info ----", a, file=sys.stderr)
         if args.stop == "Translator":
             args.outfile.write(a.translated_string)
             sys.exit()
         if args.debug:
-            with open(f"{base_file}_translated." + DEFAULT_INTER_FILE_EXT, "w+") as f:
+            with open(f"{base_file}_translated" + DEFAULT_INTER_FILE_EXT, "w+") as f:
                 f.write(a.translated_string)
         t.update_from_translator(a)
     else:
         t.tokenized_string = a.tokenized_string
+    if args.verbose:
+        print("---- Translator info ----", a, file=sys.stderr)
     t.detokenize()
     m.update_from_tokenizer(t)
     m.unmark()
@@ -73,7 +85,7 @@ def translatex(args: argparse.Namespace) -> None:
     args.outfile.write(p.unprocessed_latex)
 
 
-def main():
+def main() -> None:
     """Console script for translatex."""
     parser = argparse.ArgumentParser(description=__doc__, allow_abbrev=False)
     parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}", help="Version number")
@@ -83,7 +95,7 @@ def main():
     mutually_exclusive_group.add_argument("--dry-run", action="store_true",
                                           help="Don't translate (no API call), just run the chain of operations")
     mutually_exclusive_group.add_argument("-s", "--stop", choices=["Preprocessor", "Marker", "Tokenizer", "Translator"],
-                                          help="Stop at specified stage and write its result to output")
+                                          help="Stop at the end of the specified stage and write its result to output")
     parser.add_argument("--no-pre", action="store_false",
                         help="Don't do manual substitution during preprocessing stage")
     parser.add_argument("-mf", "--marker-format", help="Marker format to use during marking stage")
